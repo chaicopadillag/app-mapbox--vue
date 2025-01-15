368e1d472fe883e619a468386144397cd12ece09
chaicopadillag <xt9738@rimac.com.pe>
2025-01-14 20:40:21 -0500

    add router configuration and MapaView component

---
+++ src/router/index.ts
@@ -0,0 +1,15 @@
+import MapaView from '@/views/MapaView.vue'
+import { createRouter, createWebHistory } from 'vue-router'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: MapaView,
+    },
+  ],
+})
+
+export default router
---
+++ src/views/MapaView.vue
@@ -0,0 +1,3 @@
+<template>
+  <h2>Mapa vue</h2>
+</template>
